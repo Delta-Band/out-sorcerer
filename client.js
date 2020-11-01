ab24d3dcab633f3b5b6e3e4d98a059f0236665fa
ofersarid <ofersarid@gmail.com>
2020-11-01 17:47:16 +0200

    wip9

--- client.js
+++ client.js
@@ -67,44 +67,17 @@ const powerUpConfig = {
       badges.push({
         title: 'Reward',
         text: `$${reward}`,
-        color: '#32094d',
+        color: '#32094d'
       });
+    }
     if (published) {
       badges.push({
         title: 'Published',
         text: `$${published}`,
-        color: '#4d0947',
+        color: '#4d0947'
       });
     }
-    return [
-      {
-        // its best to use static badges unless you need your badges
-        // to refresh you can mix and match between static and dynamic
-        title: 'Detail Badge',
-        text: 'Static',
-        color: null
-      },
-      {
-        // card detail badges (those that appear on the back of cards)
-        // also support callback functions so that you can open for example
-        // open a popup on click
-        title: 'Popup Detail Badge',
-        text: 'Popup',
-        callback: function (t, opts) {
-          // function to run on click
-          // do something
-        }
-      },
-      {
-        // or for simpler use cases you can also provide a url
-        // when the user clicks on the card detail badge they will
-        // go to a new tab at that url
-        title: 'URL Detail Badge',
-        text: 'URL',
-        url: 'https://trello.com/home',
-        target: 'Trello Landing Page' // optional target for above url
-      }
-    ];
+    return badges;
   }
 };
 
