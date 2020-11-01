3797c3f25a292bd011540229b8d85cf06b3e5371
ofersarid <ofersarid@gmail.com>
2020-11-01 17:55:47 +0200

    wip11

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // var Promise = TrelloPowerUp.Promise;
 
 var LOGO =
-  'https://www.pinclipart.com/picdir/middle/1-18715_wizard-dog-cliparts-magician-cap-clipart-png-download.png';
+  'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
 const powerUpConfig = {
   // Start adding handlers for your capabilities here!
