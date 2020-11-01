5646bc78eeab92c4ad124fcc820fb9eee5af2528
ofersarid <ofersarid@gmail.com>
2020-11-01 15:45:58 +0200

    wip1

--- pages/publish.js
+++ pages/publish.js
@@ -64,7 +64,7 @@ export default function Publish() {
         variant='contained'
         color='primary'
         onClick={confirm}
-        disabled={true}
+        disabled={false}
       >
         Submit
       </Button>
