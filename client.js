9a03e4522a8c8b3c671d94e44b03779c4408c752
ofersarid <ofersarid@gmail.com>
2020-11-02 15:05:29 +0200

    wip2

--- client.js
+++ client.js
@@ -2,10 +2,12 @@
 // import fetch from 'fetch';
 import cardButtons from './card-buttons';
 import cardDetailBadges from './card-detail-badges';
+import cardDetailBadges from './card-badges';
 
 const powerUpConfig = {
   // Start adding handlers for your capabilities here!
   ...cardButtons,
+  ...cardDetailBadges,
   ...cardDetailBadges
 };
 
