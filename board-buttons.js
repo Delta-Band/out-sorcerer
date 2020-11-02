4414ea5492bb1cbdd59bdd069cd5261e4271945b
ofersarid <ofersarid@gmail.com>
2020-11-02 17:40:00 +0200

    wip5

--- board-buttons.js
+++ board-buttons.js
@@ -2,31 +2,25 @@ var LOGO = 'https://out-sorcerer.vercel.app/logo.png';
 
 const boardButtons = {
   'board-buttons': async function (t, options) {
-    console.log('initializig card butons');
-    const context = t.getContext();
-    console.log('context:', context);
-    const reward = await t.get(context.card, 'shared', 'reward', 0);
-    console.log('reward:', reward);
-    const published = await t.get(context.card, 'shared', 'published', false);
-    console.log('published:', published);
-    const timebox = await t.get(context.card, 'shared', 'timebox', null);
-    console.log('timebox:', timebox);
-    // const userType = await t.get('board', 'shared', 'userType', 'pusher');
-    if ((parseInt(reward, 10) === 0 || !timebox) && published) {
-      console.log('setting published to false');
-      t.set('card', 'shared', 'published', false);
-    }
-    const items = [];
-    items.push({
-      icon: {
-        dark: LOGO,
-        light: LOGO
-      },
-      text: 'OS Market',
-      url: 'https://out-sorcerer.vercel.app/market',
-      fullscreen: true
-    });
-    return items;
+    return [
+      {
+        icon: {
+          dark: LOGO,
+          light: LOGO
+        },
+        text: 'OS Market',
+        callback: function (t, opt) {
+          t.modal({
+            url: 'https://out-sorcerer.vercel.app/market',
+            fullscreen: true,
+            callback: function (t, opt) {
+              console.log('modal closed');
+              t.closeModal();
+            }
+          });
+        }
+      }
+    ];
   }
 };
 
