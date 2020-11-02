6e20e4f4f08462c117ab572b848de6a90b0c8380
ofersarid <ofersarid@gmail.com>
2020-11-02 13:21:05 +0200

    wip

---
+++ card-badges.js
@@ -0,0 +1,34 @@
+const cardDetailBadges = {
+  'card-badges': async function (t, opts) {
+    console.log('initializig card-detail-badges butons');
+    const context = t.getContext();
+    const reward = await t.get(context.card, 'shared', 'reward', 0);
+    const published = await t.get(context.card, 'shared', 'published', false);
+    const timebox = await t.get(context.card, 'shared', 'timebox', null);
+    const badges = [];
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+    });
+    badges.push({
+      title: 'Reward',
+      text: formatter.format(parseInt(reward, 10)),
+      color: reward > 0 ? 'green' : 'red'
+    });
+    badges.push({
+      title: 'Timebox',
+      text: timebox ? `${timebox} Work Days` : 'Required!',
+      color: timebox ? 'green' : 'red'
+    });
+    badges.push({
+      title: 'Published',
+      text: published ? 'YES' : 'NO',
+      color: published ? 'green' : 'red'
+    });
+    return badges;
+  }
+};
+
+export default cardDetailBadges;
