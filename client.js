12931132fcedfbc16dd975fc83703c4b5139c3bf
ofersarid <ofersarid@gmail.com>
2020-11-02 17:30:41 +0200

    wip3

--- client.js
+++ client.js
@@ -4,13 +4,15 @@ import cardButtons from './card-buttons';
 import cardDetailBadges from './card-detail-badges';
 import cardBadges from './card-badges';
 import settings from './settings';
+import boardButtons from './board-buttons';
 
 const powerUpConfig = {
   // Start adding handlers for your capabilities here!
   ...cardButtons,
   ...cardDetailBadges,
   ...cardBadges,
-  ...settings
+  ...settings,
+  ...boardButtons
 };
 
 export default powerUpConfig;
