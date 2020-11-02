912b4cedcc6da912d85c61949e376aa53a96be10
ofersarid <ofersarid@gmail.com>
2020-11-02 13:29:13 +0200

    wip5

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // var Promise = TrelloPowerUp.Promise;
 // import fetch from 'fetch';
 import cardButtons from './card-buttons';
-import cardDetailBadges from './card-detail=badges';
+import cardDetailBadges from './card-detail-badges';
 
 const powerUpConfig = {
   // Start adding handlers for your capabilities here!
