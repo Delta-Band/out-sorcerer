3deaef932475f6ea3334e111a4dddde4026daa23
ofersarid <ofersarid@gmail.com>
2020-11-03 11:55:38 +0200

    wip2

--- settings.js
+++ settings.js
@@ -3,6 +3,7 @@ const settings = {
     console.log('initializig card-detail-badges butons');
     // const context = t.getContext();
     const userType = await t.get('board', 'shared', 'userType', 'pusher');
+    console.log(userType);
     return t.modal({
       title: 'Out-Sourcer Settings',
       url: 'https://out-sorcerer.vercel.app/settings',
