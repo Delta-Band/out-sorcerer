058915b2273778fe97d18c423f786ef71a8cbaef
ofersarid <ofersarid@gmail.com>
2020-11-05 15:57:21 +0200

    wip2

--- pages/publish.js
+++ pages/publish.js
@@ -62,6 +62,7 @@ export default function Publish() {
   }, []);
 
   async function confirm() {
+    console.log('setting published: ', published ? null : Date.now());
     t.set('card', 'shared', 'published', published ? null : Date.now());
     t.notifyParent('done');
   }
