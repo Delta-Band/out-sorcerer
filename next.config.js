f2b3521b100f82c548fc066ee9f5e02e98ff10d5
ofersarid <ofersarid@gmail.com>
2020-11-05 13:05:52 +0200

    wip7

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
     TRELLO_API_KEY: process.env.TRELLO_API_KEY,
     TRELLO_API_TOKEN: process.env.TRELLO_API_TOKEN,
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY
+  },
+  optimization: {
+    minify: false
   }
 };
