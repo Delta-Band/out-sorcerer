4eb858b039f006ae57782761210aa64948dc9c03
ofersarid <ofersarid@gmail.com>
2020-11-10 15:57:09 +0200

    wip

--- card-detail-badges.js
+++ card-detail-badges.js
@@ -39,7 +39,21 @@ const cardDetailBadges = {
       badges.push({
         title: 'Claims',
         text: fireCardData.claims.length,
-        color: fireCardData.claims.length > 0 ? 'purple' : 'red'
+        color: fireCardData.claims.length > 0 ? 'purple' : 'red',
+        callback: function (t, opt) {
+          t.popup({
+            title: 'Claims',
+            url: 'https://out-sorcerer.vercel.app/claims',
+            height: 338,
+            args: {
+              claims: fireCardData.claims
+            },
+            callback: function (t, opt) {
+              t.closePopup();
+              t.set('card', 'shared', 'lastUpdate', Date.now());
+            }
+          });
+        }
       });
     }
     return badges;
---
+++ pages/claims.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import cx from 'classnames';
+import {
+  createMuiTheme,
+  ThemeProvider,
+  makeStyles,
+  withStyles
+} from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+
+const _axios = axios.create({
+  baseURL: 'https://api.trello.com/1/',
+  timeout: 1000,
+  params: {
+    key: process.env.TRELLO_API_KEY,
+    token: process.env.TRELLO_API_TOKEN
+  }
+});
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#b361fe',
+      lighter: ''
+    },
+    secondary: {
+      main: '#85ff6e'
+    }
+  }
+});
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    boxSizing: 'border-box',
+    padding: theme.spacing(1)
+  },
+  marginBottom: {
+    marginBottom: theme.spacing(2)
+  }
+}));
+
+export default function Claims() {
+  const db = firebase.firestore();
+  const [claimers, setClaimers] = useState([]);
+
+  async function getClaimers(claims) {
+    const requests = claims.reduce((acc, uid) => {
+      acc.push(() => _axios.get(`members/${uid}`));
+      return acc;
+    }, []);
+    const results = await _axios.all(requests);
+    console.log('results: ', results);
+    setClaimers(results);
+  }
+
+  useEffect(() => {
+    const _t = window.TrelloPowerUp.iframe();
+    getClaimers(_t.arg('claims'));
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography>Claimers</Typography>
+    </ThemeProvider>
+  );
+}
