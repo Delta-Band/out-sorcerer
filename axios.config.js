a5712e05624a7f28e6e9a303c1b414c2e39d66f6
ofersarid <ofersarid@gmail.com>
2020-11-12 14:39:10 +0200

    wip2

--- axios.config.js
+++ axios.config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const _axios = axios.create({
   baseURL: 'https://api.trello.com/1/',
-  timeout: 1000,
+  timeout: 5000,
   params: {
     key: process.env.TRELLO_API_KEY,
     token: process.env.TRELLO_API_TOKEN
